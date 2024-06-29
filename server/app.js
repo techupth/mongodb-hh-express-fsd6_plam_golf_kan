ed1ae39890750577f237ac043cd75872a3f182c4
Krittayapornn <krittayaporn.ww@gmail.com>
2024-06-29 15:47:18 +0700

    fix: solving exercise 1-5

--- server/app.js
+++ server/app.js
@@ -1,14 +1,18 @@
 import express from "express";
 import cors from "cors";
 import productRouter from "./apps/products.js";
-
-const app = express();
-const port = 4001;
+import {client} from "./utils/db.js"
 
 // `cors` เป็น Middleware ที่ทำให้ Client ใดๆ ตามที่กำหนด
 // สามารถสร้าง Request มาหา Server เราได้
 // ในโค้ดบรรทัดล่างนี้คือให้ Client ไหนก็ได้สามารถสร้าง Request มาหา Server ได้
-app.use(cors());
+async function init() {
+  const app = express();
+  const port = 4001;
+
+  await client.connect();
+
+  app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -22,3 +26,6 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running at port ${port}`);
 });
+
+}
+init();
