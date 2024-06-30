7dadb2ab991ca73107e1712345e61460215be616
Kan <kanalovefootball@gmail.com>
2024-06-30 21:30:10 +0700

    Creating API with MongoDB

--- server/apps/products.js
+++ server/apps/products.js
@@ -1,15 +1,76 @@
 import { Router } from "express";
+import { db } from "../utils/db";
+import { ObjectId } from "mongodb";
 
 const productRouter = Router();
 
-productRouter.get("/", (req, res) => {});
+productRouter.get("/", async (req, res) => {
+  try {
+    const collection = db.collection("products");
+    const products = await collection.limit(5).toArray();
+    return res.json({ data: products });
+  } catch (error) {
+    return res.json({
+      message: "Error",
+    });
+  }
+});
 
 productRouter.get("/:id", (req, res) => {});
 
-productRouter.post("/", (req, res) => {});
+productRouter.post("/", async (req, res) => {
+  try {
+    const collection = db.collection("products");
+    const productData = { ...req.body };
+    const products = await collection.insertOne(productData);
+    return res.json({
+      message: "Product has been created successfully”",
+    });
+  } catch {
+    return res.json({
+      message: "Error",
+    });
+  }
+});
 
-productRouter.put("/:id", (req, res) => {});
+productRouter.put("/:id", async (req, res) => {
+  try {
+    const collection = db.collection("products");
+    const productId = ObjectId(req.params.id);
+    const newProductData = { ...req.body };
+    await collection.updateOne(
+      {
+        _id: productId,
+      },
+      {
+        $set: newProductData,
+      }
+    );
+    return res.json({
+      message: "Product has been updated successfully",
+    });
+  } catch {
+    return res.json({
+      message: "Error",
+    });
+  }
+});
 
-productRouter.delete("/:id", (req, res) => {});
+productRouter.delete("/:id", async (req, res) => {
+  try {
+    const collection = db.collection("products");
+    const productId = ObjectId(req.params.productId);
+    await collection.deleteOne({
+      _id: productId,
+    });
+    return res.json({
+      message: "Product has been deleted successfully",
+    });
+  } catch {
+    return res.json({
+      message: "Error",
+    });
+  }
+});
 
 export default productRouter;
